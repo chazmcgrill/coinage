2bf28bc3150f97bee0e59fa9eeea8bc5bba0c1a0
chazmcgrill <charlietaylordev@gmail.com>
2018-02-16 09:55:24 +0000

    Styling Add Coins
    Improved add coins styling and overall look

--- src/components/BitcoinTracker.js
+++ src/components/BitcoinTracker.js
@@ -16,12 +16,13 @@ class BitcoinTracker extends Component {
         { id: 1, code: 'XRP',  name: 'Ripple',   showing: true   },
         { id: 2, code: 'LTC',  name: 'Litecoin', showing: true   },
         { id: 3, code: 'ETH',  name: 'Etherium', showing: true   },
-        { id: 4, code: 'DOGE', name: 'Dogecoin', showing: true   },
+        { id: 4, code: 'DOGE', name: 'Dogecoin', showing: false  },
         { id: 5, code: 'XMR',  name: 'Monero',   showing: true   },
         { id: 6, code: 'ZEC',  name: 'Zcash',    showing: false  },
         { id: 7, code: 'DSH',  name: 'Dash',     showing: false  },
         { id: 8, code: 'NEO',  name: 'NEO',      showing: false  },
-        { id: 9, code: 'GNT',  name: 'Golem',    showing: false  }
+        { id: 9, code: 'GNT',  name: 'Golem',    showing: false  },
+        { id: 10, code: 'ADA',  name: 'Cardano',  showing: true  }
       ]
     }
     this.updateCoins = this.updateCoins.bind(this);
