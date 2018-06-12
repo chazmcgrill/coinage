b732b4bb2c2aa7c931add0d1ffc25a06079d5061
chazmcgrill <charlietaylordev@gmail.com>
2018-06-12 15:30:44 +0100

    Fixed Footer / Add List Styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,14 +3,14 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer>
-      <p>designed and coded by <a href="https://www.charlietaylorcoder.com">charlie taylor</a></p>
+      <p>coded by <a href="https://www.charlietaylorcoder.com">charlie taylor</a></p>
       <div className="footer-icons">
         <a href="http://twitter.com/charlietcoder"><i className="fa fa-twitter"></i></a>
         <a href="http://www.instagram.com/charlietcoder"><i className="fa fa-instagram"></i></a>
         <a href="https://codepen.io/chazmcgrill"><i className="fa fa-codepen"></i></a>
         <a href="https://github.com/chazmcgrill"><i className="fa fa-github"></i></a>
       </div>
-      <p>data from <a href="https://www.cryptocompare.com/api/">cryptocompare api</a></p>
+      <p>uses <a href="https://www.cryptocompare.com/api/">cryptocompare api</a></p>
     </footer>
   )
 }
