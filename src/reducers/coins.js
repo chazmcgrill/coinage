bb40824e2084b721452bf154527462af77aaf6c2
chazmcgrill <charlietaylorcoder@gmail.com>
2019-01-09 19:44:02 +0000

    Initial Redux Setup

---
+++ src/reducers/coins.js
@@ -0,0 +1,10 @@
+const INITIAL_STATE = {
+    coins: [],
+};
+
+export default function (state = INITIAL_STATE, action) {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
