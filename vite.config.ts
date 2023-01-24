66dff5f84165c16f2b2f5257bcf03655d6614e1c
Charlie Taylor <charlie.taylor@smart-buildings.co.uk>
2023-01-24 18:47:07 +0000

    ci: further fixes base location in vite config for pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/coinage',
+    base: '/coinage/',
     test: {
         globals: true,
         environment: 'jsdom',
