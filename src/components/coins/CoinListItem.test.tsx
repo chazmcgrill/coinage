f5cb170f226e1d674d4555dc66c2c42c013057df
Charlie Taylor <charlie.taylor@smart-buildings.co.uk>
2023-01-24 18:12:42 +0000

    tests: adds tests for coin list item

---
+++ src/components/coins/CoinListItem.test.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { describe, it, vi } from 'vitest';
+import { render, fireEvent, screen } from '@testing-library/react';
+import { Coin } from '../api/coins';
+import CoinListItem from './CoinListItem';
+import * as globalStateHooks from '../global-state/hooks';
+import { ActionType } from '../global-state/types';
+
+const coin = {
+    id: 'BTC',
+    code: 'BTC',
+    name: 'Bitcoin',
+    imageURL: null,
+} as unknown as Coin;
+
+const coinPrice = {
+    GBP: '666',
+    USD: '999',
+};
+
+describe('CoinListItem', () => {
+    it('should display the coin code', () => {
+        render(<CoinListItem coin={coin} isCurrencyDollar={false} isFavourite={false} />);
+        expect(screen.getByText('BTC')).toBeTruthy();
+    });
+
+    it('should display the coin name', () => {
+        render(<CoinListItem coin={coin} isCurrencyDollar={false} isFavourite={false} />);
+        expect(screen.getByText('Bitcoin')).toBeTruthy();
+    });
+
+    it('should display the dollar coin price when it is available', () => {
+        render(<CoinListItem coin={coin} coinPrice={coinPrice} isCurrencyDollar={true} isFavourite={false} />);
+        expect(screen.getByText('$999.00')).toBeTruthy();
+    });
+
+    it('should display the sterling coin price when it is available', () => {
+        render(<CoinListItem coin={coin} coinPrice={coinPrice} isCurrencyDollar={false} isFavourite={false} />);
+        expect(screen.getByText('£666.00')).toBeTruthy();
+    });
+
+    it('should display a star icon when the coin is not a favourite', () => {
+        render(<CoinListItem coin={coin} isCurrencyDollar={false} isFavourite={false} />);
+        expect(screen.getByRole('button', { name: 'Coin favourite un-selected' })).toBeTruthy();
+    });
+
+    it('should display a filled star icon when the coin is a favourite', () => {
+        render(<CoinListItem coin={coin} isCurrencyDollar={false} isFavourite={true} />);
+        expect(screen.getByRole('button', { name: 'Coin favourite selected' })).toBeTruthy();
+    });
+
+    it('should toggle the coin as a favourite when the star icon is clicked', () => {
+        const mockDispatch = vi.fn();
+        const useDispatchSpy = vi.spyOn(globalStateHooks, 'useDispatch');
+        useDispatchSpy.mockReturnValue(mockDispatch);
+
+        render(<CoinListItem coin={coin} isCurrencyDollar={false} isFavourite={false} />);
+        fireEvent.click(screen.getByRole('button', { name: 'Coin favourite un-selected' }));
+
+        expect(mockDispatch).toHaveBeenCalledTimes(1);
+        expect(mockDispatch).toHaveBeenCalledWith({ type: ActionType.ToggleActiveCoinCode, payload: 'BTC' });
+    });
+});
