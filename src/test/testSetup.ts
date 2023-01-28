c3d177b4fb231f8623c5be2166de3e1fc4d42d70
Charlie Taylor <charlie.taylor@smart-buildings.co.uk>
2023-01-28 12:24:37 +0000

    tests: adds test config for icons and custom render wrapper

--- src/test/testSetup.ts
+++ src/test/testSetup.ts
@@ -1,4 +1,7 @@
 import matchers from '@testing-library/jest-dom/matchers';
 import { expect } from 'vitest';
+import { iconLibrarySetup } from '../utils/iconConfig';
+
+iconLibrarySetup();
 
 expect.extend(matchers);
