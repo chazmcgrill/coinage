95424fe669cae42a560295686bb7d4a50bf4545b
Charlie Taylor <charlie.taylor@smart-buildings.co.uk>
2023-01-29 11:44:41 +0000

    tests: adds tests for fetcher func with fetch mock setup

--- src/test/testSetup.ts
+++ src/test/testSetup.ts
@@ -1,7 +1,13 @@
 import matchers from '@testing-library/jest-dom/matchers';
-import { expect } from 'vitest';
+import createFetchMock from 'vitest-fetch-mock';
+import { vi, expect } from 'vitest';
 import { iconLibrarySetup } from '../utils/iconConfig';
 
 iconLibrarySetup();
+
+const fetchMock = createFetchMock(vi);
+
+// sets globalThis.fetch and globalThis.fetchMock to our mocked version
+fetchMock.enableMocks();
 
 expect.extend(matchers);
