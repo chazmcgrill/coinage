1e0ed55df800a4c1ffa0906b8fac9cac0875ca46
Charlie Taylor <charlie.taylor@smart-buildings.co.uk>
2023-12-20 14:06:43 +0000

    feat: adds local storage for persisting user theme choice

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,6 +1,8 @@
 import { useCallback, useEffect } from 'react';
 import { atom, useAtom } from 'jotai';
 
+const THEME_STORAGE_KEY = '@coinage/theme';
+
 export enum ThemeMode {
     Light = 'light',
     Dark = 'dark',
@@ -8,25 +10,32 @@ export enum ThemeMode {
 
 export const themeModeAtom = atom(ThemeMode.Dark);
 
+/** Handles color theming modes */
 const useTheme = () => {
     const [themeMode, setThemeMode] = useAtom(themeModeAtom);
 
+    /** Sets the theme mode on html attribute, local storage and state */
     const handleSetTheme = useCallback(
         (newTheme: ThemeMode) => {
             document.documentElement.setAttribute('color-mode', newTheme);
+            localStorage.setItem(THEME_STORAGE_KEY, newTheme);
             setThemeMode(newTheme);
         },
         [setThemeMode],
     );
 
+    /** Toggles theme mode between light and dark modes */
     const handleToggleThemeMode = useCallback(() => {
         const newThemeMode = themeMode === ThemeMode.Light ? ThemeMode.Dark : ThemeMode.Light;
         handleSetTheme(newThemeMode);
     }, [themeMode, handleSetTheme]);
 
+    // check previous settings and user preference and set default accordingly
     useEffect(() => {
+        const userPreviousThemeSetting = localStorage.getItem(THEME_STORAGE_KEY);
         const userPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-        const defaultTheme = userPrefersDark ? ThemeMode.Dark : ThemeMode.Light;
+        const defaultToDark = userPreviousThemeSetting === 'dark' || (userPrefersDark && !userPreviousThemeSetting);
+        const defaultTheme = defaultToDark ? ThemeMode.Dark : ThemeMode.Light;
         handleSetTheme(defaultTheme);
     }, [handleSetTheme]);
 
